5748b463da16874d6a26ac48789e72ca02011873
arnaskap <arnas.kapustinskas@gmail.com>
2016-11-24 14:57:47 +0000

    Set up firebase project, added registration and authentication (email, google, fb, twitter)

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,16 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var app  = require('express')();
+var http = require('http').Server(app);
 
 //Lets define a port we want to listen to
 const PORT=8080;
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
 
 //Lets start our server
-server.listen(PORT, function(){
+http.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
