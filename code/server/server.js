87ad85c75d332c9b6c12d474917990094b8e5e72
Juranec <juranec@gmail.com>
2016-12-06 17:50:49 +0000

    login system done

--- code/server/server.js
+++ code/server/server.js
@@ -1,4 +1,5 @@
-var app  = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 //Lets define a port we want to listen to
@@ -12,6 +13,10 @@ const PORT=8080;
 app.get('/',function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
+/*app.get('/styles/style.css',function(req,res){
+  res.sendFile(__dirname + "/styles/style.css");
+});*/
+app.use(express.static(__dirname + '/'));
 
 //Lets start our server
 http.listen(PORT, function(){
