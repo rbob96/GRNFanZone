4100fbb6f212828fcec838855082f2678d0a6538
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2017-01-10 15:21:57 +0000

    [AM] Trying to get automated tests to work

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,8 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
+    plugins: ['karma-mocha', 'karma-phantomjs-launcher'],
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
