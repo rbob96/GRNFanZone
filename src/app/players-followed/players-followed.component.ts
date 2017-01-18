87248ed44cff162fc72fe23ed799a538c83d0837
rbob96 <2126189B@student.gla.ac.uk>
2017-01-18 20:08:28 +0000

    Pls work.

--- src/app/players-followed/players-followed.component.ts
+++ src/app/players-followed/players-followed.component.ts
@@ -3,7 +3,7 @@ import { AuthService } from '../services/auth.service';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-playersFollowed',
+  selector: 'app-players',
   templateUrl: './players-followed.html',
   styleUrls: ['../players-followed/players-followed.css']
 })
--- src/app/teams-followed/teams-followed.component.ts
+++ src/app/teams-followed/teams-followed.component.ts
@@ -3,7 +3,7 @@ import { AuthService } from '../services/auth.service';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-teamsFollowed',
+  selector: 'app-teams',
   templateUrl: './teams-followed.html',
   styleUrls: ['../teams-followed/teams-followed.css']
 })
