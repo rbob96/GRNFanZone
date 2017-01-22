3da14d0d27b6db79d44f5057f630d617195a1b71
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2017-01-22 21:42:01 +0000

    [AM] remove unecessary declarations

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,9 +11,7 @@ import {Router} from '@angular/router';
 
 
 export class ProfileComponent {
-  displayName;
-  photoURL;
-  email;
+  
   constructor() {
     let displayName = '';
     let photoURL = '';
