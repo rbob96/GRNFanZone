ccda22cb64b7064df4686c5e3ad640e20e737a31
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2017-01-25 23:15:15 +0000

    [AM] Added follow() functions

--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -37,4 +37,43 @@ export class UserDataService {
     return this.af.database.list('/users/' + uid + '/players_followed');
   }
 
+  public followPlayer (userID: string, playerID: string) {
+
+    // Creates a promise for the player object, and then pushes the required data to the user's players list
+    this.af.database.object('/players/' + playerID).first().toPromise().then(player => {
+      this.af.database.list('/users/' + userID + '/players_followed').push({
+        first_name: player.first_name,
+        last_name: player.last_name,
+        id: player.id,
+        avatar: player.avatar
+      });
+    });
+
+  }
+
+  public followTeam (userID: string, teamID: string) {
+
+    this.af.database.object('/teams/' + teamID).first().toPromise().then(team => {
+      this.af.database.list('/users/' + userID + '/teams_followed').push({
+        name: team.name,
+        id: team.id,
+        avatar: team.avatar
+      });
+    });
+
+  }
+
+  public followClub (userID: string, clubID: string) {
+
+    this.af.database.object('/clubs/' + clubID).first().toPromise().then(club => {
+      this.af.database.list('/users/' + userID + '/clubs_followed').push({
+        name: club.name,
+        id: club.id,
+        avatar: club.avatar
+      });
+    });
+
+  }
+
+
 }
