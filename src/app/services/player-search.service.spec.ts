c998d7f8e9b7f100e0c16d528fb36b33b8af3651
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2017-02-03 20:29:55 +0000

    [AM] Sorted test inputs

--- src/app/services/player-search.service.spec.ts
+++ src/app/services/player-search.service.spec.ts
@@ -2,11 +2,19 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlayerSearchService } from './player-search.service';
+import {firebaseConfig} from "../app.module";
+import {AngularFireModule, AngularFire} from "angularfire2";
 
 describe('PlayerSearchService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PlayerSearchService]
+      providers: [
+        PlayerSearchService,
+        { provide : AngularFire, useClass: AngularFire }
+      ],
+      imports: [
+        AngularFireModule.initializeApp(firebaseConfig)
+      ]
     });
   });
 
