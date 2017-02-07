88e82a35da7e684d9de7ce5da0bc9b7477bf5d2f
rbob96 <2126189B@student.gla.ac.uk>
2017-02-07 20:02:28 +0000

    Misunderstood what bug was, fixed the right thing. Also fixed bootstrap issue.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:max-line-length */
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import {ClubDataService} from '../services/club-data.service';
 import {TeamDataService} from '../services/team-data.service';
 import {PlayerDataService} from '../services/player-data.service';
+
 
 
 @Component({
@@ -15,13 +16,28 @@ import {PlayerDataService} from '../services/player-data.service';
 })
 
 export class ListComponent {
+
+  private sub: any;
   clubs: FirebaseListObservable<any>;
   teams: FirebaseListObservable<any>;
   players: FirebaseListObservable<any>;
-  constructor(private clubDataService: ClubDataService, private teamDataService: TeamDataService, private playerDataService: PlayerDataService) {
+  constructor(private router: Router, private route: ActivatedRoute, private clubDataService: ClubDataService, private teamDataService: TeamDataService, private playerDataService: PlayerDataService) {
     this.clubs = this.clubDataService.getClubs();
     this.teams = this.teamDataService.getTeams();
     this.players = this.playerDataService.getPlayers();
+  }
+
+
+  public sendToPlayer (uid: string) {
+    this.router.navigate(['/players/' + uid]);
+  }
+
+  public sendToTeam (uid: string) {
+    this.router.navigate(['/teams/' + uid]);
+  }
+
+  public sendToClub (uid: string) {
+    this.router.navigate(['/clubs/' + uid]);
   }
 
 }
