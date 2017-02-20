55fd600057f3d82cd1419bd4b42e3dc1fc7cea22
rbob96 <2126189B@student.gla.ac.uk>
2017-02-20 19:00:08 +0000

    Search for clubs works.

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,69 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute, RouterModule} from '@angular/router';
 import {AngularFire, FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2';
-
-
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-
 export class ResultsComponent implements OnInit {
-
   // Get filtered results
   userData: FirebaseListObservable<any>;
   players = [];
+  teams = [];
+  clubs = [];
   users = [];
   sub: any;
-
-
   // Player uid
-  playerName: string;
-
+  searchTerm: string;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private af: AngularFire) {}
-
-
   ngOnInit() {
-
       this.sub = this.route.params.subscribe(params => {
-        this.playerName = params['query'];
+        this.searchTerm = params['query'];
         this.findPlayers();
         this.findUsers();
+        this.findTeams();
+        this.findClubs();
       });
     }
-
   findPlayers() {
-    const results = this.af.database.list('/players', {
-        query: {
-          orderByChild: 'first_name',
-          startAt: this.playerName,
-          endAt: this.playerName + '\uf8ff'
-        }
-      }).subscribe(serverResults => {
+    const results = this.af.database.list('/players').subscribe(serverResults => {
         this.players = [];
         serverResults.forEach(result => {
-          this.players.push(result);
+          if (((result.first_name + ' ' + result.last_name).toLowerCase()).includes(this.searchTerm.toLowerCase())) {
+            this.players.push(result);
+          }
         });
       });
     }
-
   findUsers() {
-    const users = this.af.database.list('/users', {
-      query: {
-        orderByChild: 'name',
-        startAt: this.playerName,
-        endAt: this.playerName + '\uf8ff'
-      }
-    }).subscribe(serverResults => {
+    const users = this.af.database.list('/users').subscribe(serverResults => {
         this.users = [];
         serverResults.forEach(result => {
-          console.log(result);
-          this.users.push(result);
+          if ((result.name.toLowerCase()).includes(this.searchTerm.toLowerCase())) {
+            this.users.push(result);
+          }
         });
       });
+  }
+  findTeams() {
+    const teams = this.af.database.list('/teams').subscribe(serverResults => {
+      this.teams = [];
+      serverResults.forEach(result => {
+        if ((result.name.toLowerCase()).includes(this.searchTerm.toLowerCase())) {
+          this.teams.push(result);
+        }
+      });
+    });
+  }
+  findClubs() {
+    const clubs = this.af.database.list('clubs').subscribe(serverResults => {
+      this.clubs = [];
+      serverResults.forEach(result => {
+        if ((result.name.toLowerCase()).includes(this.searchTerm.toLowerCase())) {
+          this.clubs.push(result);
+        }
+      });
+    });
   }
 }
