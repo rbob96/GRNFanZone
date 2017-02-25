2b4fee3312a6a23f1d5bd90eae0fcae9d68e1637
arnaskap <arnas.kapustinskas@gmail.com>
2017-02-25 14:40:56 +0000

    fixed exceptions on dashboard when no players/teams/clubs are being followed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import {AngularFire} from 'angularfire2';
 import {PostDataService} from '../services/post-data.service';
+import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 
 @Component({
@@ -17,34 +18,44 @@ export class DashboardComponent {
   shownPosts = [];
   currentUserId: string;
 
-  constructor(private postDataService: PostDataService, private authService: AuthService, private af: AngularFire) {
+  constructor(private postDataService: PostDataService,
+              private authService: AuthService,
+              private af: AngularFire,
+              private router: Router) {
 
     this.currentUserId = authService.uid;
 
       af.database.object('users/' + this.currentUserId).subscribe(userData => {
-        af.database.list('posts').subscribe(posts => {
+          af.database.list('posts').subscribe(posts => {
 
-          this.posts = [];
+            this.posts = [];
 
-          posts.forEach(post => {
+            posts.forEach(post => {
 
-            if (post.posted_by in userData.players_followed) {
-              this.posts.push(post);
-            } else if (post.posted_by in userData.teams_followed) {
-              this.posts.push(post);
-            } else if (post.posted_by in userData.clubs_followed) {
-              this.posts.push(post);
+              if (userData.players_followed != null &&
+                  post.posted_by in userData.players_followed) {
+                this.posts.push(post);
+              } else if (userData.teams_followed != null &&
+                         post.posted_by in userData.teams_followed) {
+                this.posts.push(post);
+              } else if (userData.clubs_followed != null && 
+                         post.posted_by in userData.clubs_followed) {
+                this.posts.push(post);
+              }
+            });
+            this.posts.sort((a, b) => b.created_at - a.created_at);
+            if (this.posts.length >= this.postsLimit) {
+              this.shownPostAmount += this.postsLimit;
+            } else {
+              this.shownPostAmount = this.posts.length;
             }
-        });
-        this.posts.sort((a, b) => b.created_at - a.created_at);
-        if (this.posts.length >= this.postsLimit) {
-          this.shownPostAmount += this.postsLimit;
-        } else {
-          this.shownPostAmount = this.posts.length;
-        }
-        this.shownPosts = this.posts.slice(0, this.shownPostAmount);
+            this.shownPosts = this.posts.slice(0, this.shownPostAmount);
+          });
       });
-    });
+
+  }
+
+  ngOnInit(){
 
   }
 
