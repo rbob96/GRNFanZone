b3fdc36f220444a72dda24998f90364fa69d2719
arnaskap <arnas.kapustinskas@gmail.com>
2017-02-25 19:25:28 +0000

    fixed order of event handling in dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {AngularFire} from 'angularfire2';
 import {PostDataService} from '../services/post-data.service';
 import {Router} from '@angular/router';
@@ -10,13 +10,14 @@ import {AuthService} from '../services/auth.service';
 })
 
 
-export class DashboardComponent {
+export class DashboardComponent{
 
   posts = [];
   postsLimit = 2;
   shownPostAmount = 0;
   shownPosts = [];
   currentUserId: string;
+  userData;
 
   constructor(private postDataService: PostDataService,
               private authService: AuthService,
@@ -24,27 +25,25 @@ export class DashboardComponent {
               private router: Router) {
 
     this.currentUserId = authService.uid;
-      af.database.object('users/' + this.currentUserId).subscribe(userData => {
           af.database.list('posts').subscribe(posts => {
 
             this.posts = [];
 
             posts.forEach(post => {
-
-              if (userData.players_followed != null &&
-                  post.posted_by in userData.players_followed) {
+              if (this.userData.players_followed != null &&
+                  post.posted_by in this.userData.players_followed) {
                 this.posts.push(post);
-              } else if (userData.teams_followed != null &&
-                         post.posted_by in userData.teams_followed) {
+              } else if (this.userData.teams_followed != null &&
+                         post.posted_by in this.userData.teams_followed) {
                 this.posts.push(post);
-              } else if (userData.clubs_followed != null &&
-                         post.posted_by in userData.clubs_followed) {
+              } else if (this.userData.clubs_followed != null &&
+                         post.posted_by in this.userData.clubs_followed) {
                 this.posts.push(post);
               }
             });
-            if (this.posts.length === 0 && router.url === '/') {
-              router.navigate(['results']);
-            }
+            if (this.posts.length === 0 && this.router.url === '/') {
+              this.router.navigate(['results']);
+            }     
             this.posts.sort((a, b) => b.created_at - a.created_at);
             if (this.posts.length >= this.postsLimit) {
               this.shownPostAmount += this.postsLimit;
@@ -53,8 +52,13 @@ export class DashboardComponent {
             }
             this.shownPosts = this.posts.slice(0, this.shownPostAmount);
           });
-      });
 
+  }
+
+  ngOnInit(){
+    this.af.database.object('users/' + this.currentUserId).subscribe(userData => {
+      this.userData = userData;
+    });
   }
 
   addComment(newComment: string, postid: string ) {
