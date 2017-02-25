2341b81f5fd7a101f5e671045ff1d315e65a97d9
Juranec <juranec@gmail.com>
2017-02-25 15:20:10 +0000

    language list in navbar, "Language" translations TODO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
-import {TranslateService} from 'ng2-translate';
-
 
 @Component({
     selector: 'app-gnr',
@@ -10,13 +8,7 @@ import {TranslateService} from 'ng2-translate';
 })
 
 export class AppComponent {
-    // By default, only the TranslateStaticLoader is available.
-    // It will search for files in i18n/*.json,
-    // if you want you can customize this behavior by changing the default
-    // prefix/suffix:
+    constructor(private af:	AuthService) {
 
-
-    constructor(private af:	AuthService, public translate: TranslateService) {
-      translate.setDefaultLang('en');
     }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import {TranslateService} from 'ng2-translate';
 
 
 @Component({
@@ -10,7 +11,8 @@ import { AuthService } from '../services/auth.service';
 
 export class NavbarComponent {
   currentUser;
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, public translate: TranslateService) {
     this.currentUser = authService.userDetails;
+    translate.setDefaultLang('en');
   }
 }
